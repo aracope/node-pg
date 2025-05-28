52cfc52eca6826b9fa7f6b8549b46d20aa3e9cee
aracope <156398622+aracope@users.noreply.github.com>
2025-05-28 15:44:50 -0700

    Step 1: add biztime database, load data.sql, configure db.js

--- db.js
+++ db.js
@@ -1,2 +1,18 @@
 /** Database setup for BizTime. */
+const { Client } = require("pg");
+
+let DB_URI;
+if (process.env.NODE_ENV === "test") {
+  DB_URI = "postgresql:///biztime_test";
+} else {
+  DB_URI = process.env.DATABASE_URL || "postgresql:///biztime";
+}
+
+const db = new Client({
+  connectionString: DB_URI
+});
+
+db.connect();
+
+module.exports = db;
 
